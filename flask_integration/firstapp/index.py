04dd3d303dbaefa70d205398959a6732086831d4
nat <nathanc1945@gmail.com>
2018-12-31 09:00:07 -0400

    Created pure js version

--- flask_integration/firstapp/index.py
+++ flask_integration/firstapp/index.py
@@ -9,8 +9,51 @@ from astropy.coordinates import Angle
 
 app = Flask(__name__)
 
-safeZone = {"safeZone": [ [0, -20], [6, 10], [6, 90], [0, 90], [0, 60], [-4, 40], [-5, 40], [-4, 10], [-3.5, 5], [-3,0], [-1,-10]] }
+safeZone = {"safeZone":[
+                [0, -20],
+                [15, -18],
+                [30, -16],
+                [45, -14],
+                [60, -10],
+                [75, 0],
+                [90, 10],
+                [90,20],
+                [90,30],
+                [90,40],
+                [90,50],
+                [90,60],
+                [90,70],
+                [75,70],
+                [75,90],
+                [60, 90],
+                [45,90],
+                [30,90],
+                [15,90],
+                [0, 90],
+                [0,80],
+                [0,70],
+                [0,60],
+                [0,50],
+                [-15, 50],
+                [-30, 50],
+                [-45,50],
+                [-60,50],
+                [-60, 40],
+                [-75, 40],
+                [-75,20],
+                [-67.5,15],
+                [-60, 10],
+                [-52.5, 5],
+                [-45,0],
+                [-37.5,-5],
+                [-30,-5],
+                [-22.5,-10],
+                [-15,-10],
+                [-7.5,-10],
+                [-7.5,-10],
+                [0,-20]] }
 
+#safeZone = {"safeZone": [[0,90],[60,90],[60,-40],[0,-40],[0,90]]}
 
 @app.route('/')
 def hello_world():
@@ -65,6 +108,6 @@ def getSiderealTime(date='12/25/2018', coords='48.4284N, 123.3656W', reps=1, int
     st = st + ' hours'
     a = Angle(st)
     st = a.to_string(unit=u.degree, decimal=True, sep=':')
-    st = {"siderealtime": float(st)}
+    st = {"siderealtime": st}
     # Looks like 08:52:17.8084; we'll create an angle object from this time
     return jsonify(st)
